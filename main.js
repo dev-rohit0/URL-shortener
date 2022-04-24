58c1f6daed453b5a6dfc811afad5382cd47dc0a3
dev-rohit0 <93703830+dev-rohit0@users.noreply.github.com>
2022-04-24 15:02:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let longLink = document.querySelector('#longLink');
+let btn  = document.querySelector('#btn');
+let output  = document.querySelector(".output");
+
+
+btn.addEventListener('click',function(){
+  let url = longLink.value;
+  fetch(' https://api.shrtco.de/v2/shorten?url='+url)
+  .then(response => response.json())
+  .then(data =>{
+    console.log(data['result'])
+    
+    document.getElementById("shortLink").value = (data['result']['full_short_link']);
+  })
+})
+
+document.getElementById('copy').addEventListener("click", function(){
+  shortLink.select();
+  document.execCommand('copy');
+  alert('Link Copied');
+});
